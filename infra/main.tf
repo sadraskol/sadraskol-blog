55e47b5de7006cb42213d66cd4092859c9e5fce0
Thomas Bracher <thomas.bracher@doctolib.com>
2022-11-20 19:10:38 +0100

    :shrug:

--- infra/main.tf
+++ infra/main.tf
@@ -238,7 +238,7 @@ data "aws_iam_policy_document" "github_oid_assume_role_policy" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:sadraskol/sadraskol-blog:*"]
+      values   = ["repo:sadraskol/sadraskol-blog:dev"]
     }
   }
 }
